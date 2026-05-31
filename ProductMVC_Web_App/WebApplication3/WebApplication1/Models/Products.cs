b49ec8dc84c2b7d53351278e57f3a9a19e3b1fda
vedprtpsingh <singhvedpratap14@gmail.com>
2026-05-31 16:04:01 +0530

    Updated project files

---
+++ ProductMVC_Web_App/WebApplication3/WebApplication1/Models/Products.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models
+{
+    public class Products
+    {
+
+
+        public int Id { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
