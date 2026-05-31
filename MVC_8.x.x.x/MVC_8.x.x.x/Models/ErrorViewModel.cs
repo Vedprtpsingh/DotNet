c50c4d09cd233daa94c5d4ac2bd6e84401778941
vedprtpsingh <singhvedpratap14@gmail.com>
2026-05-31 19:07:38 +0530

    Updated project files

--- MVC_8.x.x.x/MVC_8.x.x.x/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MVC_8.x.x.x.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
